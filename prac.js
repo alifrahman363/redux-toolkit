cc055527054f2e33a2a06b33a2e1ad85f88e8079
alif <alifrahman363@gmail.com>
2023-12-30 12:55:52 +0600

    practice fully

---
+++ prac.js
@@ -0,0 +1,67 @@
+// state - count:0
+// action - increment, decrement, reset
+// reducer
+// store
+
+const { createStore } = require('redux');
+
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+const RESET = 'RESET';
+
+const initialState = {
+    count: 0
+}
+
+const incrementAction = () => {
+    return {
+        type: INCREMENT
+    }
+}
+
+const decrementAction = () => {
+    return {
+        type: DECREMENT
+    }
+}
+
+const resetAction = () => {
+    return {
+        type: RESET
+    }
+}
+
+// create reducer
+const counterReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case INCREMENT:
+            return {
+                ...state,
+                count: state.count + 1
+            }
+        case DECREMENT:
+            return {
+                ...state,
+                count: state.count - 1
+            }
+        case RESET:
+            return {
+                ...state,
+                count: 0
+            }
+        default:
+            state;
+
+    }
+}
+
+// Store
+const store = createStore(counterReducer);
+store.subscribe(() => {
+    console.log("Changing state: ", store.getState());
+})
+
+store.dispatch(incrementAction());
+store.dispatch(incrementAction());
+store.dispatch(decrementAction());
+store.dispatch(resetAction());
